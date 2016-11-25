718c24af834feffe84b91934411709df0f6d4023
Akash Chandra <akashchandra733@gmail.com>
2016-11-25 14:21:15 +0530

    project01 data

---
+++ app/src/main/java/com/example/akashchandra/project01/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.akashchandra.project01;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akash Chandra on 25-11-2016.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+    }
+}
